9d17ec90a7b1f13f0c11066263e0ff2acc99e907
NamKey <kakapo12@naver.com>
2020-07-08 08:54:29 +0900

    View update
    -View
    1. Fragment 구현
    2. BottomNavigationView 구현

---
+++ app/src/main/java/com/example/macarondiary/MainActivity.kt
@@ -0,0 +1,97 @@
+/* 진행상황
+1. View
+    - Fragment
+    - BottomNavigationView
+2. Function
+
+3. Communication
+
+*/
+
+/* 특이사항
+2020.07.08->bottomNavigation object의 setOnNavigationItemSeletected Method를 BottomNavigaionView의 OnNavigationSelected Method로 Overriding
+
+*/
+
+
+package com.example.macarondiary
+
+
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        //View 선언
+        var bottomNavigation: BottomNavigationView? = null
+        bottomNavigation = findViewById(R.id.bottom_navigation)
+
+        //Fragment 선언
+        val homefragment = HomeFragment()
+        val diaryfragment = DiaryFragment()
+        val shopfragment = ShopFragment()
+        val settingfragment = SettingFragment()
+
+
+
+        bottomNavigation.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener {item:MenuItem ->
+            when(item.itemId){
+                R.id.btn_fragment_gohome->{
+                    Log.d("Home","homebtn")
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.fragment_layout,homefragment).commitAllowingStateLoss();
+                    }
+                    true
+                }
+                R.id.btn_fragment_godiary->{
+                    Log.d("Home","diarybtn")
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.fragment_layout,diaryfragment).commitAllowingStateLoss();
+                    }
+                    true
+                }
+                R.id.btn_fragment_goshop->{
+                    Log.d("Home","shopbtn")
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.fragment_layout,shopfragment).commitAllowingStateLoss();
+                    }
+                    true
+                }
+                R.id.btn_fragment_gosetting->{
+                    Log.d("Home","settingbtn")
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.fragment_layout,settingfragment).commitAllowingStateLoss();
+                    }
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        })
+    }//CreateView End
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/macarondiary/ShopFragment.kt
@@ -0,0 +1,23 @@
+package com.example.macarondiary
+
+import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ShopFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+
+    }
+}
