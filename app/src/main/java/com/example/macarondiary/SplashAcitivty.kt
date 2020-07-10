0b4f5a3471c1f8646538ec459f205fed6cea7238
NamKey <kakapo12@naver.com>
2020-07-11 00:28:17 +0900

    RecyclerView 수정
    -View
    1. RecyclerView item xml 수정
    2. SplashActivity 추가

---
+++ app/src/main/java/com/example/macarondiary/SplashAcitivty.kt
@@ -0,0 +1,34 @@
+package com.example.macarondiary
+
+import android.animation.*
+import android.animation.Animator.AnimatorListener
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.PersistableBundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.lang.Exception
+
+class SplashAcitivty : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val mainImagesplash = findViewById<ImageView>(R.id.splash_imageView)
+
+        //Animation Splash
+
+        //No Animation splash
+        Handler().postDelayed(Runnable {
+            val intentGomain = Intent(this,MainActivity::class.java)
+            startActivity(intentGomain)
+            finish()
+        },3000)
+
+
+    }
+}
